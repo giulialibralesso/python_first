edaea86984c74e459e19d144e7bc0c6a7b795ba9
giulia_lib <44606898+giulialibralesso@users.noreply.github.com>
2021-08-25 16:59:06 +0200

    Update dictnames.py

--- Python_tryings/Python_tryings/dictnames.py
+++ Python_tryings/Python_tryings/dictnames.py
@@ -1,6 +1,6 @@
 counts = dict()
 names = ['Anna', 'Alice', 'Franco', 'Berto', 'Lucia', 'Elisa', 'Lucia', 'Berto']
-for name in names[:5] :
+for name in names #[:5] :
     if name not in counts :
         counts[name] = 1
     else :
