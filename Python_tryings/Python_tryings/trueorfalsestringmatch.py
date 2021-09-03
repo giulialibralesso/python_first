b8640c1c738e36775e081cdd1ba5bc7baed23c8d
giulia_lib <44606898+giulialibralesso@users.noreply.github.com>
2021-09-03 14:58:34 +0200

    Update trueorfalsestringmatch.py

--- Python_tryings/Python_tryings/trueorfalsestringmatch.py
+++ Python_tryings/Python_tryings/trueorfalsestringmatch.py
@@ -3,7 +3,6 @@ handle = open(name, 'r')
 wantedstring = 'cat'
 
 def detect (string, file_content) :
-    for line in file_content :
         if string not in file_content :
             result = False
         else :
